7ba42245fddfdf68a21363f99a02463bc2a09e61
alexlee-dev <alex@alexlee.dev>
2019-10-20 11:33:28 -0700

    Delete and Add Buttons in Dev mode. And abstract things in index.js to App.js.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import Input from "./components/Input";
 import Output from "./components/Output";
 import styled from "styled-components";
 import Status from "./components/status";
 import Home from "./components/Home";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Store from "electron-store";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { StylesProvider, ThemeProvider } from "@material-ui/core/styles";
 
 const AppContainer = styled.div`
   align-items: flex-start;
@@ -14,13 +19,41 @@ const AppContainer = styled.div`
   width: 100%;
 `;
 
-const App = ({ store }) => {
+const store = new Store();
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      500: "#e040fb"
+    }
+  }
+});
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const Content = styled.div`
+  flex-grow: 1;
+`;
+
+const InnerContainer = styled.div`
+  height: calc(100vh - 164px);
+  margin-left: 240px;
+  padding: 50px;
+`;
+
+const App = () => {
   const [output, setOutput] = useState(null);
   const [stage, setStage] = useState("home");
   const [file, setFile] = useState(null);
   const [converted, setConverted] = useState(false);
   const [displayStatus, setDisplayStatus] = useState(false);
   const [type, setType] = useState(null);
+  const [, updateState] = React.useState();
+  const forceUpdate = useCallback(() => updateState({}), []);
 
   const stages = {
     home: <Home store={store} />,
@@ -48,20 +81,33 @@ const App = ({ store }) => {
     )
   };
 
+  // * If this is the first time the app is run, store an empty array.
   useEffect(() => {
     if (!store.get("pastFiles")) {
       store.set("pastFiles", []);
     }
-  }, [store]);
+  }, []);
 
   return (
-    <AppContainer id="app-container">
-      {stages[stage]}
-      <Status
-        displayStatus={displayStatus}
-        setDisplayStatus={setDisplayStatus}
-      />
-    </AppContainer>
+    <StylesProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <Container id="main-container">
+          <Content id="content">
+            <Header />
+            <Sidebar forceUpdate={forceUpdate} store={store} />
+            <InnerContainer id="inner-container">
+              <AppContainer id="app-container">
+                {stages[stage]}
+                <Status
+                  displayStatus={displayStatus}
+                  setDisplayStatus={setDisplayStatus}
+                />
+              </AppContainer>
+            </InnerContainer>
+          </Content>
+        </Container>
+      </ThemeProvider>
+    </StylesProvider>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,53 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-import { StylesProvider, ThemeProvider } from "@material-ui/core/styles";
-import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
-import { createMuiTheme } from "@material-ui/core/styles";
-import styled from "styled-components";
 import "./assets/prism.css";
-import Store from "electron-store";
 
-const store = new Store();
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      500: "#e040fb"
-    }
-  }
-});
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-`;
-
-const Content = styled.div`
-  flex-grow: 1;
-`;
-
-const InnerContainer = styled.div`
-  height: calc(100vh - 164px);
-  margin-left: 240px;
-  padding: 50px;
-`;
-
-ReactDOM.render(
-  <StylesProvider injectFirst>
-    <ThemeProvider theme={theme}>
-      <Container id="main-container">
-        <Content id="content">
-          <Header />
-          <Sidebar />
-          <InnerContainer id="inner-container">
-            <App store={store} />
-          </InnerContainer>
-        </Content>
-      </Container>
-    </ThemeProvider>
-  </StylesProvider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
